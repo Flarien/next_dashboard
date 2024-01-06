5787f385a42c4461943b8fc283f8a7aab20f03e6
Flavia S. Briglia <fbrig87@gmail.com>
2024-01-06 16:25:38 -0300

    iniciando proyecto dashboard con Next js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import './ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        {/* children es el contenido de la pagina */}
+      </body>
     </html>
   );
 }
