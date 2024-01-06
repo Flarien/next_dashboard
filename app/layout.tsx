dac2ae9021982d227d2013c1205fa857618b3e20
Flavia S. Briglia <fbrig87@gmail.com>
2024-01-06 19:15:01 -0300

    creando primeras pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { montserrat } from './ui/fonts';
 import './ui/global.css';
 
 export default function RootLayout({
@@ -7,9 +8,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={`${montserrat.className} antialiased`}>
         {children}
         {/* children es el contenido de la pagina */}
+        <footer className='py-10 flex justify-center items-center'>
+          Esto es el footer
+        </footer>
       </body>
     </html>
   );
